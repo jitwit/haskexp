8e58856b93f0d7eda6f3c4b1e9ff360e1a08e4bb
jitwit <jrn@bluefarm.ca>
2019-06-19 15:42:43 -0400

    array and name change

--- Language/HExp.hs
+++ Language/HExp.hs
@@ -6,7 +6,8 @@ import Data.String
 import Data.Word
 import Data.List
 import Data.Foldable
-import Data.Vector (Vector)
+import Data.Vector (Vector,fromList)
+import Data.Array (Array,listArray,bounds,elems)
 import Data.Map (Map)
 import Data.Set (Set)
 import Data.IntSet (IntSet)
@@ -19,25 +20,26 @@ put_hexp :: SEXP -> IO ()
 put_hexp = putStrLn . show
 
 he1 :: SEXP
-he1 = HExpList [HExpInteger 1, HExpInteger 2]
+he1 = SList [SInteger 1, SInteger 2]
 he2 :: SEXP
-he2 = HExpChar 'x'
+he2 = SChar 'x'
 he3 :: SEXP
-he3 = HExpList [HExpChar 'x', HExpChar 'y']
+he3 = SList [SChar 'x', SChar 'y']
 he4 :: SEXP
-he4 = HExpList [ HExpSymbol "cat"
-               , HExpList [HExpString "bird", HExpString "wolf"]]
-he5 = HExpCons he2 he3
-he6 = HExpCons (HExpString "cat") (HExpInteger 12)
-he7 = HExpRatio (1 :% 3)
-he8 = HExpComplex (1 :+ 3)
-he9 = HExpComplex (1 :+ (-3))
-he10 = HExpComplex (1 :+ 0)
+he4 = SList [ SSymbol "cat"
+            , SList [SString "bird", SString "wolf"]]
+he5 = SCons he2 he3
+he6 = SCons (SString "cat") (SInteger 12)
+he7 = SRatio (1 :% 3)
+he8 = SComplex (1 :+ 3)
+he9 = SComplex (1 :+ (-3))
+he10 = SComplex (1 :+ 0)
+he11 = sexp_of (fromList [1,2,3] :: Vector Float)
 
 hv1 = sexp_of @(Int,Integer) (12,13)
 hv2 = sexp_of [1..10 :: Int]
-hv3 = HExpQuote hv1
-hv4 = HExpQuasiQuote hv1
+hv3 = SQuote hv1
+hv4 = SQuasiQuote hv1
 hv5 = sexp_of ()
 hv6 = sexp_of ((), (1 :+ 2) :: Complex Double, (1 :% 123) :: Rational)
 t1,t2 :: Tree Int
@@ -45,95 +47,102 @@ t1 = Node 1 []
 t2 = Node 3 [Node 4 [Node 6 [], Node 8 []], Node 5 []]
 hv7 = sexp_of t1
 hv8 = sexp_of t2
-hv9 = HExpCons hv2 he4
-hv10 = HExpCons hv2 he7
-hv11 = HExpCons hv2 (HExpList [he7])
+hv9 = SCons hv2 he4
+hv10 = SCons hv2 he7
+hv11 = SCons hv2 (SList [he7])
+a1 :: Array Int Int
+a1 = listArray (0,2) [1..3]
+ha1 = sexp_of a1
 
-hexps1 = [he1,he2,he3,he4,he5,he6,he7,he8,he9,he10
-         ,hv1,hv2,hv3,hv4,hv5,hv6,hv7,hv8,hv9,hv10,hv11]
+hexps1 = [he1,he2,he3,he4,he5,he6,he7,he8,he9,he10,he11
+         ,hv1,hv2,hv3,hv4,hv5,hv6,hv7,hv8,hv9,hv10,hv11
+         ,ha1]
 
 ghcid = do traverse_ put_hexp hexps1
 
 data SEXP where
-  HExpBool :: Bool -> SEXP
-  HExpInteger :: Integer -> SEXP
-  HExpDouble :: Double -> SEXP
-  HExpFloat :: Float -> SEXP
-  HExpComplex :: (Ord h, Num h, Show h) => Complex h -> SEXP
-  HExpRatio :: Show h => Ratio h -> SEXP
-  HExpChar :: Char -> SEXP
-  HExpString :: String -> SEXP
-  HExpSymbol :: String -> SEXP
-  HExpList :: [SEXP] -> SEXP
-  HExpCons :: SEXP -> SEXP -> SEXP
-  HExpVector :: Vector SEXP -> SEXP
-  HExpQuote :: SEXP -> SEXP
-  HExpQuasiQuote :: SEXP -> SEXP
+  SBool :: Bool -> SEXP
+  SInteger :: Integer -> SEXP
+  SDouble :: Double -> SEXP
+  SFloat :: Float -> SEXP
+  SComplex :: (Ord h, Num h, Show h) => Complex h -> SEXP
+  SRatio :: Show h => Ratio h -> SEXP
+  SChar :: Char -> SEXP
+  SString :: String -> SEXP
+  SSymbol :: String -> SEXP
+  SList :: [SEXP] -> SEXP
+  SCons :: SEXP -> SEXP -> SEXP
+  SVector :: Vector SEXP -> SEXP
+  SQuote :: SEXP -> SEXP
+  SQuasiQuote :: SEXP -> SEXP
 
 instance Show SEXP where
   show = \case
-    HExpList xs -> '(' : (unwords $ show <$> xs) <> ")"
-    HExpBool b -> if b then "#t" else "#f"
-    HExpInteger x -> show x
-    HExpChar x -> "#\\" <> [x]
-    HExpString x -> show x
-    HExpSymbol x -> x
-    HExpDouble x -> show x
-    HExpFloat x -> show x
-    HExpRatio (x :% y) -> concat [show x,"/",show y]
-    HExpComplex (x :+ y) ->
+    SList xs -> '(' : (unwords $ show <$> xs) <> ")"
+    SBool b -> if b then "#t" else "#f"
+    SInteger x -> show x
+    SChar x -> "#\\" <> [x]
+    SString x -> show x
+    SSymbol x -> x
+    SDouble x -> show x
+    SFloat x -> show x
+    SRatio (x :% y) -> concat [show x,"/",show y]
+    SComplex (x :+ y) ->
       if | y > 0 -> concat [show x,"+",show y,"i"]
          | y < 0 -> concat [show x,"-",show (- y),"i"]
          | otherwise -> show x
-    HExpCons x y -> case y of
-      HExpList ys -> show (HExpList (x:ys))
+    SCons x y -> case y of
+      SList ys -> show (SList (x:ys))
       _ -> '(' : unwords [show x, ".", show y]  <> ")"
-    HExpVector v -> '#' : show (HExpList (toList v)) 
-    HExpQuote x -> '\'' : show x
-    HExpQuasiQuote x -> '`' : show x
+    SVector v -> '#' : show (SList (toList v)) 
+    SQuote x -> '\'' : show x
+    SQuasiQuote x -> '`' : show x
 
 class SEXPOf h where
   sexp_of :: h -> SEXP
 
 instance SEXPOf () where
-  sexp_of () = HExpList []
+  sexp_of () = SList []
 
 instance SEXPOf Bool where
-  sexp_of = HExpBool
+  sexp_of = SBool
 
 instance SEXPOf Char where
-  sexp_of = HExpChar
+  sexp_of = SChar
 
 instance SEXPOf Int where
-  sexp_of = HExpInteger . fromIntegral
+  sexp_of = SInteger . fromIntegral
 
 instance SEXPOf Word where
-  sexp_of = HExpInteger . fromIntegral
+  sexp_of = SInteger . fromIntegral
 
 instance SEXPOf Integer where
-  sexp_of = HExpInteger
+  sexp_of = SInteger
 
 instance SEXPOf Double where
-  sexp_of = HExpDouble
+  sexp_of = SDouble
+  
+instance SEXPOf Float where
+  sexp_of = SFloat
   
 instance (Show v, Num v, Ord v, SEXPOf v) => SEXPOf (Complex v) where
-  sexp_of = HExpComplex
+  sexp_of = SComplex
 
 instance (Show v, SEXPOf v) => SEXPOf (Ratio v) where
-  sexp_of = HExpRatio
+  sexp_of = SRatio
 
 instance SEXPOf h => SEXPOf [h] where
-  sexp_of = HExpList . map sexp_of
+  sexp_of = SList . map sexp_of
 
 instance (SEXPOf a, SEXPOf b) => SEXPOf (a,b) where
-  sexp_of (x,y) = HExpCons (sexp_of x) (sexp_of y)
+  sexp_of (x,y) = SCons (sexp_of x) (sexp_of y)
 
 instance (SEXPOf a, SEXPOf b, SEXPOf c) => SEXPOf (a,b,c) where
-  sexp_of (x,y,z) = HExpList [sexp_of x, sexp_of y, sexp_of z]
+  sexp_of (x,y,z) = SList [sexp_of x, sexp_of y, sexp_of z]
 
 instance (SEXPOf a, SEXPOf b, SEXPOf c, SEXPOf d)
       => SEXPOf (a,b,c,d) where
-  sexp_of (x,y,z,w) = HExpList [sexp_of x, sexp_of y, sexp_of z, sexp_of w]
+  sexp_of (x,y,z,w) = SList [sexp_of x, sexp_of y, sexp_of z, sexp_of w]
 
 instance (SEXPOf k, SEXPOf v) => SEXPOf (Map k v) where
   sexp_of = sexp_of . toList
@@ -145,20 +154,26 @@ instance SEXPOf v => SEXPOf (Set v) where
   sexp_of = sexp_of . toList
 
 instance SEXPOf h => SEXPOf (Vector h) where
-  sexp_of = HExpVector . fmap sexp_of
+  sexp_of = SVector . fmap sexp_of
+
+instance (SEXPOf i, SEXPOf h) => SEXPOf (Array i h) where
+  -- not sure what to do here
+  sexp_of xs = SList [SSymbol "array"
+                     ,sexp_of (bounds xs)
+                     ,sexp_of (fromList (elems xs))]
 
 instance SEXPOf h => SEXPOf (Tree h) where
   sexp_of (Node x []) = sexp_of x
-  sexp_of (Node x xs) = HExpCons (sexp_of x) (sexp_of xs)
+  sexp_of (Node x xs) = SCons (sexp_of x) (sexp_of xs)
 
 instance Semigroup SEXP where
-  HExpList xs <> HExpList ys = HExpList (xs <> ys)
-  HExpList xs <> y = if null xs then y else HExpList (xs <> [y])
-  x <> HExpList ys = if null ys then x else HExpList (x:ys)
-  x <> y = HExpCons x y
+  SList xs <> SList ys = SList (xs <> ys)
+  SList xs <> y = if null xs then y else SList (xs <> [y])
+  x <> SList ys = if null ys then x else SList (x:ys)
+  x <> y = SCons x y
 
 instance Monoid SEXP where
-  mempty = HExpList []
+  mempty = SList []
 
   
 --  a@Atom{} <> b@Atom{} = List [a,b]
